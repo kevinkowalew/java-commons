0f4a44a857152e72d3a2683e3c88f6216c0ba746
Kevin Kowalewski <kowalewski.ke@gmail.com>
2020-09-28 04:45:07 -0600

    adds integration tests for PostgresqlConnection

---
+++ databases/src/test/java/databases/PostgresqlConnectionIntegrationTests.java
@@ -0,0 +1,28 @@
+package databases;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+public class PostgresqlConnectionIntegrationTests {
+    @Test
+    public void testDockerContainerConnection() {
+        // Arrange
+        PostgresqlConnection connection = PostgresqlConnection.newBuilder()
+                .setHost("localhost")
+                .setPort(5432)
+                .setDatabaseName("database")
+                .setUser("admin")
+                .setPassword("admin")
+                .build();
+
+        // Act
+        Optional<DatabaseController> controller = DatabaseController.connect(connection);
+
+        // Assert
+        Assert.assertTrue(controller.isPresent());
+        Assert.assertTrue(controller.get().isOpen());
+    }
+}
